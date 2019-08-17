8d6e56e11012989d8fc19cfb7100ee4d64be2ac6
Szymon Wicki <szymon.wicki@gmail.com>
2019-08-17 15:19:20 +0200

    Working app

--- src/main/java/com/sda/projectcurrencygame/controller/CurrencyClient.java
+++ src/main/java/com/sda/projectcurrencygame/controller/CurrencyClient.java
@@ -1,8 +1,6 @@
 package com.sda.projectcurrencygame.controller;
 
 import com.sda.projectcurrencygame.model.CurrencyModel;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
 
